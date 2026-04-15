9848f8f23cec45883862be79c5979a8d7c4faf51
Abu3wa <abu3wa.abdulhafeez@example.com>
2026-04-15 17:17:51 +0200

    fix: add missing useState import in AssignmentsPage

--- client/src/pages/assignments/AssignmentsPage/AssignmentsPage.jsx
+++ client/src/pages/assignments/AssignmentsPage/AssignmentsPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
